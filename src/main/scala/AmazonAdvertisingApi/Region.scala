fa806b1c760aecfc29bee4004d56ca4db71a58ec
Toan Nguyen <nguyentoandev@gmail.com>
2019-04-09 16:28:43 +0700

    add Region

---
+++ src/main/scala/AmazonAdvertisingApi/Region.scala
@@ -0,0 +1,27 @@
+package AmazonAdvertisingApi
+
+import java.net.URL
+
+abstract class Region {
+  val production: URL
+  val sandbox: URL
+  val tokenUrl: URL
+}
+
+case object NA extends Region {
+   val production: URL = new URL("https://advertising-api.amazon.com")
+   val sandbox: URL = new URL("https://advertising-api-test.amazon.com")
+   val tokenUrl: URL = new URL("https://api.amazon.com/auth/o2/token")
+}
+
+case object EU extends Region {
+  val production: URL = new URL("https://advertising-api-eu.amazon.com")
+  val sandbox: URL = new URL("https://advertising-api-test.amazon.com")
+  val tokenUrl: URL = new URL("https://api.amazon.com/auth/o2/token")
+}
+
+case object FE extends Region {
+  val production: URL = new URL("https://advertising-api-fe.amazon.com")
+  val sandbox: URL = new URL("https://advertising-api-test.amazon.com")
+  val tokenUrl: URL = new URL("https://api.amazon.com/auth/o2/token")
+}
