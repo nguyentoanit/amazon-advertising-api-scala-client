a7b618cf20df4506c1725b53f9126b47f0ba62a7
Toan Nguyen <nguyentoandev@gmail.com>
2019-04-09 14:21:14 +0700

    Refresh token

--- src/main/scala/AmazonAdvertisingApi/Client.scala
+++ src/main/scala/AmazonAdvertisingApi/Client.scala
@@ -5,18 +5,27 @@ import play.api.libs.json._
 import java.net.URL
 
 class Client (clientId: String, clientSecret: String, region: String, accessToken: String, refreshToken: String, sandbox: Boolean = false) {
-  def buildRequest(method: String, url: URL, headers: Seq[(String, String)], bodies: JsValue = JsValue()): HttpRequest = {
+  def buildRequest(method: HTTPMethod, url: URL, headers: Seq[(String, String)], body: JsValue): HttpRequest = {
     val request = Http(url.toString).headers(headers)
-    method.toUpperCase() match {
-      case "GET" => request
-      case "POST" => request.postData(Json.stringify(bodies))
+    method match {
+      case GET => request
+      case POST => request.postData(Json.stringify(body))
     }
   }
 
   def doRefreshToken: Unit = {
-
+    val headers = Seq()
+    val body: JsValue = Json.obj(
+      "grant_type" -> "refresh_token",
+      "refresh_token" -> this.refreshToken,
+      "client_id" -> this.clientId,
+      "client_secret" -> this.clientSecret
+    )
+    val url: URL = new URL("https://api.amazon.com/auth/o2/token")
+    this.buildRequest(POST, url, headers, body)
   }
 }
+
 object Client {
   def apply(config: Config): Client = new Client(config.clientId, config.clientSecret, config.region, config.accessToken, config.refreshToken)
 }
---
+++ src/main/scala/AmazonAdvertisingApi/HTTPMethod.scala
@@ -0,0 +1,5 @@
+package AmazonAdvertisingApi
+
+class HTTPMethod
+case object GET extends HTTPMethod
+case object POST extends HTTPMethod
