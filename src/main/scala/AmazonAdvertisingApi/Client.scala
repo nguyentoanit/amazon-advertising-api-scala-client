52e16e776691a3b48fce0a5ff81e982c246d96b9
Toan Nguyen <nguyentoandev@gmail.com>
2019-04-11 10:51:59 +0700

    Bind profileId to get report function

--- src/main/scala/AmazonAdvertisingApi/Client.scala
+++ src/main/scala/AmazonAdvertisingApi/Client.scala
@@ -6,7 +6,6 @@ import java.net.URL
 
 class Client (clientId: String, clientSecret: String, refreshToken: String, region: Region, version: String, sandbox: Boolean = false) {
   var accessToken: String = ""
-  var profileId: String = ""
   val domain: String = if (sandbox) this.region.sandbox.toString else this.region.production.toString
 
   private def buildRequest(method: HTTPMethod, url: URL, headers: Seq[(String, String)], body: JsValue): HttpRequest = {
@@ -39,17 +38,17 @@ class Client (clientId: String, clientSecret: String, refreshToken: String, regi
     }
   }
 
-  def requestReport(reportType: String, data: JsValue): HttpRequest = this._operation(reportType + "/report", POST, data)
+  def requestReport(reportType: String, profileId: String, data: JsValue): HttpRequest = this._operation(reportType + "/report", profileId, POST, data)
 
-  def getReport(reportId: String): URL = {
-    val request = this._operation(s"reports/$reportId").asString
+  def getReport(reportId: String, profileId: String): URL = {
+    val request = this._operation(s"reports/$reportId", profileId).asString
     val response: JsValue = Json.parse(request.body)
     (response \ "status").asOpt[String] match {
-      case Some(status) if status == "SUCCESS" => this._download(s"reports/$reportId/download")
+      case Some(status) if status == "SUCCESS" => this._download(s"reports/$reportId/download", profileId)
       case Some(status) if status == "IN_PROGRESS" =>
         // Pause 5 seconds before check status again
         Thread.sleep(5000)
-        this.getReport(reportId)
+        this.getReport(reportId, profileId)
       case Some(status) if status == "FAILURE" =>
         val statusDetails = (response \ "statusDetails").asOpt[String].getOrElse("Description is Not Found!")
         throw new Exception(s"$status: $statusDetails")
@@ -60,21 +59,20 @@ class Client (clientId: String, clientSecret: String, refreshToken: String, regi
     }
   }
 
-  private def _download(path: String): URL = {
-    val request = this._operation(path).asString
+  private def _download(path: String, profileId: String): URL = {
+    val request = this._operation(path, profileId).asString
     val downloadLink: String = request.header("Location").get
     new URL(downloadLink)
   }
 
-  private def _operation(path: String, method: HTTPMethod = GET, body: JsValue = Json.obj()): HttpRequest = {
+  private def _operation(path: String, profileId: String = "", method: HTTPMethod = GET, body: JsValue = Json.obj()): HttpRequest = {
     var headers: Seq[(String, String)] = Seq(
       "Content-Type" -> "application/json",
       "Authorization" -> s"Bearer ${this.accessToken}",
       "Amazon-Advertising-API-ClientId" -> this.clientId
     )
 
-    if (!this.profileId.trim.isEmpty) headers = headers :+ ("Amazon-Advertising-API-Scope" -> this.profileId)
-    else throw new Exception("Profile ID is not found!")
+    if (!profileId.trim.isEmpty) headers = headers :+ ("Amazon-Advertising-API-Scope" -> profileId)
 
     val url = new URL(this.domain + "/" + this.version + "/" + path)
     this.buildRequest(method, url, headers, body)
